f42ea8c783c8f7353cafbdab46d743f3114d1a1a
Valeria <valeria_korzun@mail.ru>
2022-11-28 17:00:54 +0300

    Taks 6

---
+++ Task_06/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()); 
+
+if(n % 2 == 0)
+    Console.Write("Число четное");
+else
+    Console.Write("Число нечетное");
