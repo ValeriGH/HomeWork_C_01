a36b93fb9999e5ced5c1ca2dd94bc25a0bede098
Valeria <valeria_korzun@mail.ru>
2022-11-28 17:00:42 +0300

    Taks 4

---
+++ Task_04/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int n = int.Parse(Console.ReadLine()); 
+
+Console.Write("Введите 2-е число: ");
+int m = int.Parse(Console.ReadLine()); 
+
+Console.Write("Введите 3-е число: ");
+int k = int.Parse(Console.ReadLine()); 
+
+if (n > m && n > k)
+    Console.Write($"max = {n}");
+else if (m > k)
+        Console.Write($"max = {m}");
+    else 
+        Console.Write($"max = {k}");
