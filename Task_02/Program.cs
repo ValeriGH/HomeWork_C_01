d4596426077b1883c821f11b51107a304e50cf62
Valeria <valeria_korzun@mail.ru>
2022-11-28 17:00:30 +0300

    Taks 2

---
+++ Task_02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int n = int.Parse(Console.ReadLine()); 
+
+Console.Write("Введите 2-е число: ");
+int m = int.Parse(Console.ReadLine()); 
+
+if (n > m)
+    Console.Write($"max = {n}");
+else
+    Console.Write($"max = {m}");
